5b02321420146904a0e7d7fbb22da6726c2d8aba
jbucky86 <47157305+jbucky86@users.noreply.github.com>
2019-02-26 19:36:04 -0800

    Update PiRaqCompanion.sh

--- PiRaqCompanion.sh
+++ PiRaqCompanion.sh
@@ -49,7 +49,7 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --disable-session-crashed-bubble --force-device-scale-factor=0.28 --kiosk http://127.0.0.1:8000/tablet.html
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --disable-session-crashed-bubble --disable-session-crashed-bubble --force-device-scale-factor=0.28 --kiosk http://127.0.0.1:8000/tablet.html
 
 while true; do
    xdotool keydown ctrl+r; xdotool keyup ctrl+r;
