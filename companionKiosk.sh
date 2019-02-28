0b8cc52bb98b638876b5c7dd74b761a742dd7f13
jbucky86 <47157305+jbucky86@users.noreply.github.com>
2019-02-28 09:03:37 -0800

    Update companionKiosk.sh

--- companionKiosk.sh
+++ companionKiosk.sh
@@ -10,7 +10,7 @@ sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --disable-session-crashed-bubble --disable-session-crashed-bubble --force-device-scale-factor=0.28 --kiosk http://127.0.0.1:8000/tablet.html
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --disable-session-crashed-bubble --force-device-scale-factor=0.28 --kiosk http://127.0.0.1:8000/tablet.html
 
 while true; do
    xdotool keydown ctrl+r; xdotool keyup ctrl+r;
